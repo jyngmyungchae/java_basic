ec616470f3d64550f5cfb6858705a0b87e0f48da
jyngmyungchae <jyngmyungchae@gmail.com>
2025-01-30 13:23:14 +0900

    java basic

---
+++ src/NullPointerException/Data.java
@@ -0,0 +1,5 @@
+package NullPointerException;
+
+public class Data {
+    int value;
+}
---
+++ src/NullPointerException/Main.java
@@ -0,0 +1,9 @@
+package NullPointerException;
+
+public class Main {
+    public static void main(String[] args) {
+        Data data = null;
+        data.value = 10; // NullPointerException 예외 발생
+        System.out.println(data.value);
+    }
+}
